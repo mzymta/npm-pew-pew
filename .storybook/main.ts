dce300c338247f924f676f5ae3350acf52b74268
Evgenii Koniakhin <evgenii.koniakhin@clyso.com>
2025-02-07 18:15:16 +0100

    chore: storybook relative paths
    Signed-off-by: Evgenii Koniakhin <evgenii.koniakhin@clyso.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,26 @@
-import type { StorybookConfig } from "@storybook/vue3-vite";
+import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
-  staticDirs: [{from: '../src/assets', to: '/assets'}],
+  staticDirs: [{ from: '../src/assets', to: 'assets' }],
   framework: {
-    name: "@storybook/vue3-vite",
+    name: '@storybook/vue3-vite',
     options: {},
   },
   core: {
     disableWhatsNewNotifications: true,
+  },
+  async viteFinal(c) {
+    // Set base to './' to make paths relative
+    c.base = './';
+
+    return c;
   },
 };
 
